1d07e634543961e1a7d684f28691554162cf6506
Darwin <darwinu34@yahoo.es>
2021-06-02 17:59:56 -0500

    se agrega la conexion

--- Saludar.EntitiesDto/IdiomaEntityDto.cs
+++ Saludar.EntitiesDto/IdiomaEntityDto.cs
@@ -1,9 +1,11 @@
 ﻿namespace Saludar.EntitiesDto
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
 
     public class IdiomaEntityDto
     {
+        [Key]
         public Guid IdIdioma { get; set; }
 
         public string DescripcionIdioma { get; set; }
