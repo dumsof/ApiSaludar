2c4e7f5e7a3d47f96483e1ed4611795577bfe904
Darwin <darwinu34@yahoo.es>
2021-06-04 06:25:56 -0500

    se saca swagger de la validación de ambiente de desarrollo.

--- ApiSaludar/Startup.cs
+++ ApiSaludar/Startup.cs
@@ -75,12 +75,14 @@ namespace ApiSaludar
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("../swagger/v1/swagger.json", "Api Saludar v1"));
+
             if (env.IsDevelopment())
             {
                 IdentityModelEventSource.ShowPII = true;
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("swagger/v1/swagger.json", "Api Saludar v1"));
+                
             }
 
             app.UseRouting();
