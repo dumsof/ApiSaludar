94497a3b5dcf5a8ce39fa9b08d655261df7a4dd1
Darwin <darwinu34@yahoo.es>
2021-06-04 10:57:12 -0500

    se agrega proyecto de pruebas

---
+++ ApiSaludar.Pruebas/Business/AccionBotonBusinessTest.cs
@@ -0,0 +1,18 @@
+namespace ApiSaludar.Pruebas.Business
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AccionBotonBusinessTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+        }
+            [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+    }
+}
